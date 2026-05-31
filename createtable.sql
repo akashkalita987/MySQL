1c011b1c59cb2b98547ae7154389427e7626bbbf
akashkalita987 <akashjyotikalita76@gmail.com>
2026-05-31 20:29:29 +0530

    creating table using the database

--- createtable.sql
+++ createtable.sql
@@ -4,6 +4,14 @@ create database employee_data;
 
 /*using the database*/
 
+use employee_data;
+
 /*Table creation*/
 
-create table data
+create table data(
+    empID int primary key auto_increment,
+    First_name varchar(15) not null,
+    mid_name varchar(15),
+    last_name varchar(20) not null,
+    age int not null,
+);
